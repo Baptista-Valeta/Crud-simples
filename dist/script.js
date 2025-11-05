3aed1c0ce7f5fbb1c59dd8617dd85fdf806c50be
Baptista Valeta <gabryelvaleta@gmail.com>
2025-11-05 14:32:27 +0100

    Funcão do butão para salvar e listar os dados dos clientes

--- dist/script.js
+++ dist/script.js
@@ -14,30 +14,42 @@ document.querySelector(".icon-close").addEventListener("click", () => {
 
 
 
-function cadastrar() {
-   const nome = document.querySelector('#nome');
-   const numero = document.querySelector('#numero');
-   const morada = document.querySelector('#morada');
+// function cadastrar() {
+//    const nome = document.querySelector('#nome');
+//    const numero = document.querySelector('#numero');
+//    const morada = document.querySelector('#morada');
 
-   if (nome.value === '' || numero.value === '' || morada.value === '') {
-      const caixaError = document.querySelector('.error').style.display = 'block';
+//    if (nome.value === '' || numero.value === '' || morada.value === '') {
+//       const caixaError = document.querySelector('.error').style.display = 'block';
       
-      // Adicionar a mensagem de erro ...
+//       // Adicionar a mensagem de erro ...
 
-   }else {
-      const table = document.getElementsByTagName("tbody")[0];
-      const table_list = document.createElement("tr");
+//    }else {
+//       const table = document.getElementsByTagName("tbody")[0];
+//       const table_list = document.createElement("tr");
    
-      console.log(nome.value, numero.value, morada.value)
+//       console.log(nome.value, numero.value, morada.value)
    
-      table_list.innerHTML = `
-         <td>${nome.value}</td>
-         <td>${numero.value}</td>
-         <td>${morada.value}</td>
-         <></>
-      `;
+//       table_list.innerHTML = `
+//          <td>${nome.value}</td>
+//          <td>${numero.value}</td>
+//          <td>${morada.value}</td>
+//          <></>
+//       `;
    
-      table.appendChild(table_list);
+//       table.appendChild(table_list);
 
+//    }
+// }
+
+const save = document.querySelector("#save").addEventListener("click", () => {
+   const nome = document.querySelector('#nome').value;
+   const numero = document.querySelector('#numero').value;
+   const morada = document.querySelector('#morada').value;
+   const email = document.querySelector("#email").value;
+
+   if(!nome || !numero || !morada || !email) {
+      alert ("Dados incompletos");
    }
-}
+});
+
