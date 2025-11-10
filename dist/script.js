f96c6cd0beee8054e93422a0a16d310b52db39d1
Baptista Valeta <gabryelvaleta@gmail.com>
2025-11-11 00:45:53 +0100

    Validação do formulário completa.

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
-const modal_cadastro =    document.querySelector('.section1');
+const modal_cadastro = document.querySelector('.section1');
 
-window.document.querySelector("#openCadastro").addEventListener("click", function() {
+window.document.querySelector("#openCadastro").addEventListener("click", function openCadastro() {
    modal_cadastro.classList.add('active'); // Adiciona a classe active ao alemento   
    container.classList.add("active");
 });
@@ -23,7 +23,7 @@ const validarForm = () => {
    let isValid = true;
 
    // Validar nome
-   if (!nome.value) {
+   if (!nome.value.trim()) {
       nome.style.borderColor = "red";
       erro[0].textContent = "*O nome é obrigatório!";
       erro[0].classList.add("active");
@@ -38,29 +38,80 @@ const validarForm = () => {
       nome.focus();
       isValid = false;
    }else {
-      nome.style.borderColor="green";
+      nome.style.borderColor="lightgreen";
       erro[0].classList.remove("active");
    }
 
    // Validar numero
-   if(numero.value.length !== 9) {
+   if (!numero.value.trim()) {
       numero.style.borderColor = "red";
-      erro.textContent = "O número deve ter 9 car"
+      erro[1].textContent = "*O número é obrigatório!";  
+      erro[1].classList.add("active");      
+      isValid = false;
+   }else if(numero.value.length !== 9) {
+      numero.style.borderColor = "red";
+      erro[1].textContent = "O número deve ter 9 dígitos!"
+      erro[1].classList.add("active");      
+      isValid = false;
    }else {
-      numero.style.borderColor = "green"; 
+      numero.style.borderColor = "lightgreen"; 
+      erro[1].classList.remove("active");
    }
 
-   return true;
+   // Validar morada
+   if (!morada.value.trim()) {
+      morada.style.borderColor = "red";
+      erro[2].textContent = "*A morada é obrigatória!";
+      erro[2].classList.add("active");
+      isValid = false;
+   }else if (morada.value.length < 4) {
+      morada.style.borderColor = "red";
+      erro[2].textContent = "*A morada deve conter pelo menos 4 caracteres!"
+      erro[2].classList.add("active");
+      isValid = false;
+   }else {
+      morada.style.borderColor = "lightgreen";
+      erro[2].classList.remove("active");
+   }
+
+   // Validar email
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   if (!email.value.trim()) {
+      email.style.borderColor = "red";
+      erro[3].textContent = "*O email é obrigatório!"
+      erro[3].classList.add("active");
+
+      isValid = false;
+   }else if (!emailRegex.test(email.value.trim())) {
+      email.style.borderColor = "red";
+      erro[3].textContent = "*Insira um email válido por favor!"
+      erro[3].classList.add("active");
+
+      isValid = false;
+   }else {
+      email.style.borderColor = "lightgreen";
+      erro[3].classList.remove("active");
+   }
+   return isValid;
 }
 
 const salvarDados = (e) => {
    e.preventDefault(); // Impede o envio padrão do formulário
    if (e.type === "click") {
-      if(validarForm() === true) {
-         console.log("Pronto para enviar os dados preenchidos!");
+      if(validarForm() !== false) {
+         
+         // Mensagem de sucesso
+         let sucess = document.querySelector('.sucessy');
+         sucess.textContent = `Cliente ${nome.value} cadastrado com sucesso!`;
+         sucess.classList.add("active");
+         setInterval(() => {
+            sucess.classList.remove("active");
+         }, 3500);
+    
+
 
       }else {
-         console.error("Dados incompletos!")
+         // alert("Dados incompletos!")
       }
    }
 
